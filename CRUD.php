abf82f39660798b721db3719fa41d0e825f073f9
Abhishek <abhishekdhar1991@gmail.com>
2022-06-05 16:55:42 +0530

    added data tables // commit via VS Code

--- CRUD.php
+++ CRUD.php
@@ -51,6 +51,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
       href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
       crossorigin="anonymous"
+      <link rel="stylesheet" href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+      <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     />
 
     <title>Project 1 - PHP CRUD</title>
